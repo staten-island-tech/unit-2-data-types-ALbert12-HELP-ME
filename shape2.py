68b191422fe3187ede06bb6854f8847ccef0106a
ALbert12-HELP-ME <kep873291@gmail.com>
2025-02-06 09:26:18 -0500

    commit

---
+++ shape2.py
@@ -0,0 +1,9 @@
+print("test")
+#def defines a function 
+#inside the parenthesis are the inputs aka arguments 
+def add(x,y):
+    print(x+y)
+    return(x+y)
+    #print(x+y)
+    #return creates an output for the function 
+print(add(5+6))
