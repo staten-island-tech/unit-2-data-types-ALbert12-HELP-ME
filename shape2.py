cfdd39e835bb0b53c604d69c6fc8e920c5c84b7e
ALbert12-HELP-ME <kep873291@gmail.com>
2025-02-07 09:26:10 -0500

    conditional statements

--- shape2.py
+++ shape2.py
@@ -1,4 +1,4 @@
-print("test")
+""" print("test")
 #def defines a function 
 #inside the parenthesis are the inputs aka arguments 
 def add(x,y):
@@ -6,4 +6,49 @@ def add(x,y):
     return(x+y)
     #print(x+y)
     #return creates an output for the function 
-print(add(5+6))
+print(add(5+6)) """
+
+""" x = 3
+y = float(3)
+print(x,y) """
+
+""" values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i)
+print(values[0])
+print(values[6])
+ """
+""" x = "this is a thing"
+y= x.split( )
+z = y[0]
+print(y)
+print(z) """
+
+""" day_of_week = input("what day is it? ")
+if day_of_week == "Friday":
+    print("correct")
+else:
+    print("incorrect") """
+
+""" x = "test"
+print(f"hello {x}")
+ """
+""" 
+temp = 75
+if temp > 68:
+    print('warm')
+elif temp == 68:
+    print('perfect')
+else:
+    print('cold') """
+
+
+
+
+
+
+
+
+
+
