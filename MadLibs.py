1774fe12ee87b4f72b69cd27f6002f7ed7ef70d9
ALbert12-HELP-ME <kep873291@gmail.com>
2025-02-14 09:06:39 -0500

    MadLibs

--- MadLibs.py
+++ MadLibs.py
@@ -9,6 +9,13 @@ verb8 = input("Enter a verb")
 verb9 = input("Enter a verb:")
 verb10 = input("Enter a verb")
 noun1 = input("Enter a noun: ")
+noun2 = input("Enter a noun: ")
+noun3 = input("Enter a noun: ")
+noun4 = input("Enter a noun: ")
+noun5 = input("Enter a noun: ")
 number = input("Enter a number: ")
 celebrity = input("Enter the name of a celebrity: ")
 
+MadLibs = f"Welcome to {celebrity} "
+
+print (MadLibs)
