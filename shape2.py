d0b647d6b79e8fabe3e9d9093d12ade6d55713eb
ALbert12-HELP-ME <kep873291@gmail.com>
2025-02-25 09:25:08 -0500

    conditional statements

--- shape2.py
+++ shape2.py
@@ -81,6 +81,27 @@ service_rating = input("How was the service? (bad, okay, good, great): ").lower(
 
 calculate_tip(bill, service_rating) """
 
+""" x = True #is a factor 
+y = True 
 
+def GCF(x,y):
+    if x == True and y == True:
+        print("common factor") """
+""" def vote(age, id):
+    if age < 18 or id == False:
+        print("cannot vote")
+    elif age > 18 and id == True:
+        print("you can vote") """
 
+""" def skins(money, age, isAvailable):
+    if money < 10 or age < 18 or isAvailable == False:
+        return("cannot buy") """
+""" def skins(money, cost, isAvailable):
+    if isAvailable == True:
+        if money >10 or cost == 0:
+            print("can purchase")
+        else:
+            print("Lucas Broke Boy")
+    else:
+        print("not available")  """
 
